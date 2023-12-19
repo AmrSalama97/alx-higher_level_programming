8e00885e445fd715b063aecfc5633c4b322452a2
AmrSalama97 <amrsalama97@gmail.com>
2023-12-19 12:11:00 -0800

    Classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
-class Square:
-    pass
+#!/usr/bin/python3
+"""Square generation module for Python project 0x06
+"""
 
+
+class Square:
+    """class defined for square generation
+    """
+    pass
